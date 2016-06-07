e70a495d433ff1b6a14ae415fd786e13edd0d929
jvieira <jvieira@f80ad51f-db79-4307-b24f-aec1c37bd7ba>
2016-06-07 09:38:56 +0000

    Added script to update the solr index for every file in the inscriptions directory.
    git-svn-id: file:///project/repositories/svn2/iospe/trunk@3907 f80ad51f-db79-4307-b24f-aec1c37bd7ba

---
+++ kiln/index.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+SERVER_URL="http://localhost:9999/admin/solr/index/tei/tei/inscriptions"
+SLEEP_TIME=30
+
+while getopts ":s:w:" optval "$@"
+do
+    case $optval in
+        "s")
+            SERVER_URL="$OPTARG"
+            ;;
+        "w")
+            SLEEP_TIME="$OPTARG"
+            ;;
+        *)
+            errormsg="Unknown parameter or option error with option - $OPTARG"
+            echo $errormsg
+            exit -1
+            ;;
+    esac
+done
+
+counter=0
+
+for f in webapps/ROOT/content/xml/tei/inscriptions/*.xml
+do
+    counter=$((counter + 1))
+
+    filename=$(basename "$f")
+    filename="${filename%.*}"
+
+    if [[ $filename =~ ^[5P]{1}.*$ ]]; then
+        echo $SERVER_URL/$filename.html
+        wget --timeout=0 $SERVER_URL/$filename.html
+    fi
+
+    if [[ $((counter % 100)) = 0 ]]; then
+        echo "Pausing for $SLEEP_TIME second(s)..."
+        sleep $SLEEP_TIME
+    fi
+done
